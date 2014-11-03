676c5dbaa00839d71b5e96f7f970723cb4816776
Anjo Vahldiek <vahldiek@mpi-sws.org>
2014-11-03 14:36:59 +0100

    lab2

--- lock_client.cc
+++ lock_client.cc
@@ -30,12 +30,10 @@ lock_client::stat(lock_protocol::lockid_t lid)
 lock_protocol::status
 lock_client::acquire(lock_protocol::lockid_t lid)
 {
-  return lock_protocol::RPCERR;
 }
 
 lock_protocol::status
 lock_client::release(lock_protocol::lockid_t lid)
 {
-  return lock_protocol::RPCERR;
 }
 
